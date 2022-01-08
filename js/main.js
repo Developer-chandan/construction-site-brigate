8e992904c0800e925e5250b8cd9d43c8a3032e98
Chandan Sen <chandan2441139@gmail.com>
2022-01-08 22:43:06 +0600

    construction website template

---
+++ js/main.js
@@ -0,0 +1,147 @@
+/////service box section
+$(document).ready(function(){
+  $('#serive-info').waypoint(function(direction){
+    $('.service-card-animate').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'70%'
+  })
+})
+
+///work with team
+$(document).ready(function(){
+  $('#work-with-team').waypoint(function(direction){
+    $('.work-team-animate-image').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#work-with-team').waypoint(function(direction){
+    $('.work-team-animate-info').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'70%'
+  })
+})
+////////achievement///
+
+
+
+$(document).ready(function(){
+  $('#achievement').waypoint(function(direction){
+    $('.achievement-box').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'70%'
+  })
+})
+/////more service ////
+
+$(document).ready(function(){
+  $('#more-services').waypoint(function(direction){
+    $('.service-title-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#more-services').waypoint(function(direction){
+    $('.more-service-box').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'60%'
+  })
+})
+
+//////price plan///
+$(document).ready(function(){
+  $('#price-plan').waypoint(function(direction){
+    $('.price-plan-card-animate').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'70%'
+  })
+})
+
+////our-latest-idea//
+
+$(document).ready(function(){
+  $('#our-latest-idea').waypoint(function(direction){
+    $('.idea-image-layer').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#our-latest-idea').waypoint(function(direction){
+    $('.idea-title').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'70%'
+  })
+})
+$(document).ready(function(){
+  $('#our-latest-idea').waypoint(function(direction){
+    $('.idea-box-wrapper').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'60%'
+  })
+})
+
+
+//////our-team///
+
+$(document).ready(function(){
+  $('#our-team').waypoint(function(direction){
+    $('.team-title-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#our-team').waypoint(function(direction){
+    $('.team-box').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'60%'
+  })
+})
+
+///////our best projects////
+$(document).ready(function(){
+  $('#our-best-projects').waypoint(function(direction){
+    $('.best-project-title-Animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#our-best-projects').waypoint(function(direction){
+    $('.project-boxes').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'55%'
+  })
+})
+/////// our-letest-news///
+$(document).ready(function(){
+  $('#our-letest-news').waypoint(function(direction){
+    $('.latest-news-title-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#our-letest-news').waypoint(function(direction){
+    $('.news-box-animate').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'60%'
+  })
+})
+////////////
+$(document).ready(function(){
+  $('#subscribe-now').waypoint(function(direction){
+    $('.subscribe-box-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'70%'
+  })
+})
